0a5d6db65b88209706fb60ba3972d7eb2ba23c61
Kacper <56998046+DoboszKacper@users.noreply.github.com>
2020-02-26 09:43:31 +0100

    Docker Files
    These are Docker Files for the databases(HolidayApi and UserAPi).

---
+++ Dockerfile-way/holidays.sql
@@ -0,0 +1,5 @@
+create table holidays (holiday_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT,place TEXT, datee DATE);
+insert into holidays (user_id,place,datee) values (2 , 'Malediwy','2017-06-15');
+insert into holidays (user_id,place,datee) values (3 , 'Poland','2016-12-29');
+insert into holidays (user_id,place,datee) values (5 , 'New York','2012-09-01');
+
---
+++ Dockerfile-way/users.sql
@@ -0,0 +1,6 @@
+create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, phone_number TEXT);
+insert into users (name,phone_number) values ('Kacper' , '667667202');
+insert into users (name,phone_number) values ('Michal' , '423234653');
+insert into users (name,phone_number) values ('Kamil' , '6734583443');
+insert into users (name,phone_number) values ('Janek' , '231253575');
+insert into users (name,phone_number) values ('Grzegorz' , '123456789');
